d336933a7f20236ecdacd2f1ad61c2951171be46
Alessandro Giovanni Polanski <55267153+alessandropolanski@users.noreply.github.com>
2020-10-25 18:56:15 -0300

    Add files via upload

---
+++ backend/src/models/Ongs.ts
@@ -0,0 +1,23 @@
+import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity('ongs')
+export default class Ong {
+  @PrimaryGeneratedColumn('increment')
+  id: number;
+  @Column()
+  name:string;
+  @Column()
+  latitude: number;
+  @Column()
+  longitude: number;
+  @Column()
+  about: string;
+  @Column()
+  instructions: string;
+  @Column()
+  opening_hours: string;
+  @Column()
+  open_on_weekends:boolean;
+
+  
+}
